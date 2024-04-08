9b5209437c774f5ce21752fa261cb16b660c53c7
Egor <dementev.eg@yandex.ru>
2024-04-08 14:16:41 +0300

    add getters in user dto

--- src/services/dto/User.js
+++ src/services/dto/User.js
@@ -1,14 +1,54 @@
 export class User {
     constructor(id, username, realName, surname, dateOfBirth, phoneNumber, isOnline, patronymic, profilePhotoId, thumbnailPhotoId) {
-        this._id = id;
-        this._username = username;
-        this._realName = realName;
-        this._surname = surname;
-        this._dateOfBirth = dateOfBirth;
-        this._phoneNumber = phoneNumber;
-        this._isOnline = isOnline;
-        this._patronymic = patronymic;
-        this._profilePhotoId = profilePhotoId;
-        this._thumbnailPhotoId = thumbnailPhotoId;
+        this.id = id;
+        this.username = username;
+        this.realName = realName;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.isOnline = isOnline;
+        this.patronymic = patronymic;
+        this.profilePhotoId = profilePhotoId;
+        this.thumbnailPhotoId = thumbnailPhotoId;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getUsername() {
+        return this.username;
+    }
+
+    getRealName() {
+        return this.realName;
+    }
+
+    getSurname() {
+        return this.surname;
+    }
+
+    getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    getIsOnline() {
+        return this.isOnline;
+    }
+
+    getPatronymic() {
+        return this.patronymic;
+    }
+
+    getProfilePhotoId() {
+        return this.profilePhotoId;
+    }
+
+    getThumbnailPhotoId() {
+        return this.thumbnailPhotoId;
     }
 }
